1a2006b2c1e36988b410088a6db8b5f34f4c0ec3
Baha <ukonat@gmail.com>
2016-09-30 19:33:56 +0900

    Assert of variables and rules

--- rev.pl
+++ rev.pl
@@ -3,10 +3,27 @@
 
 :- use_module(library(tokenize)).
 
+:- dynamic(vars/1).
+:- dynamic(rule/3).
+
+
 parse :- 
   tokenize_file('example.trs',Tokens,[cased(true),spaces(false)]),
   lists:subtract(Tokens,[cntrl('\n')],CleanToks),
   %write(CleanToks), % for tokenizer debugging
   phrase(program(X),CleanToks),
-  prettyTRS(X).
+  prettyTRS(X),
+  assertTRS(X).
   
+assertTRS(ctrs(V,R)) :-
+  R =.. [rules|Rs],
+  assertVars(V),
+  assertRules(Rs).
+
+assertVars(V) :- assertz(V).
+
+assertRules([]).
+assertRules([R|Rs]) :-
+  assertz(R),
+  assertRules(Rs).
+
