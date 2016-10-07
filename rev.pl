446d1efcd6e9446de1328ef34dd8081651da8202
Baha <ukonat@gmail.com>
2016-10-07 16:02:38 +0900

    Implemented flattening for rhs

--- rev.pl
+++ rev.pl
@@ -16,10 +16,9 @@ parse :-
   vars(T,Vs),
   funs(T,Fs),
   post(T,Vs,Fs,PT),
-  pretty(PT).
-%  assertTRS(PT).
-%  prettyTRS(X),
-%  assertTRS(X).
+  pretty(PT),
+  flatten_ctrs(PT,FT),
+  pretty(FT).
   
 assertTRS(ctrs(_,R)) :-
   R = rules(Rs),
@@ -78,33 +77,86 @@ post(rule(B,X,Y,Z),Vs,Fs,rule(B2,X2,Y2,Z2),N) :-
   post(Z,Vs,Fs,Z2),
   post(B,Vs,Fs,B2,N).
 
-%flatten(Rule,Flatrule) :-
-%  \+ is_flat(Rule),
-%  flatten_rule(Rule,Flatrule).
-%
-%flatten(Rule,Rule) :-
-%  is_flat(Rule).
+flatten_ctrs(ctrs(V,rules(R)),ctrs(V,rules(R2))) :-
+  flatten_rules(R,R2).
 
-%is_flat(rule(_,_,R,C)) :-
-%  (is_basic(R);is_cons(R)),
+flatten_rules([],[]).
+flatten_rules([R|Rs],[R2|Rs2]) :-
+  flatten_rule(R,R2),
+  flatten_rules(Rs,Rs2).
 
-%flatten_rule(rule(B,L,R,C),rule(B,L2,R2,C2)) :-
-%  flatten_
+flatten_rule(rule(B,L,R,C),rule(B,L,R2,C3)) :-
+  flatten_rhs(R,R2,NewCs),
+  append(C,NewCs,C3).%C2).%,
+%  flatten_conds(C2,C3).
 
-is_basic(rule(_,_,R,C)) :-
-  is_basic(R),
-  is_basic(C).
-is_basic(cond(L,R)) :-
-  is_basic(L),
-  is_basic(R).
-is_basic(fun(_,Ts)) :-
-  is_cons(Ts).
+flatten_rhs(T,T2,Cs) :-
+  flatten_top(T,T2,Cs).
 
-is_cons([]).
-is_cons([T|Ts]) :-
+flatten_top(T,T,[]) :-
+  T = fun(_,_),
+  is_basic(T).
+flatten_top(T,T,[]) :-
+  T = cons(_,_),
+  is_cons(T).
+flatten_top(T,T,[]) :-
+  T = var(_,_),
+  is_cons(T).
+flatten_top(T,T2,Cs) :-  
+  T = fun(N,Ts),
+  \+ is_basic(T),
+  flatten_bot(Ts,Ts2,Cs),
+  T2 = fun(N,Ts2).
+flatten_top(T,T2,Cs) :-  
+  T = cons(N,Ts),
+  \+ is_cons(T),
+  flatten_bot(Ts,Ts2,Cs),
+  T2 = cons(N,Ts2).
+
+
+flatten_bot([],[],[]).
+flatten_bot([T|Ts],[T|Ts2],Cs) :-
   is_cons(T),
-  is_cons(Ts).
-is_cons(cons(_,[])).
-is_cons(cons(_,Ts)) :-
-  is_cons(Ts).
+  flatten_bot(Ts,Ts2,Cs).
+flatten_bot([T|Ts],[T2|Ts2],[C|Cs2]) :-
+  \+ is_cons(T),
+  flatten_bot(T,T2,C),
+  flatten_bot(Ts,Ts2,Cs2).
+flatten_bot(T,T2,C) :-
+  T = fun(_,_),
+  \+ is_cons(T),
+  fresh_var(T2),
+  C = cond(T,T2).
+flatten_bot(T,T2,C) :-
+  T = cons(_,_),
+  \+ is_cons(T),
+  fresh_var(T2),
+  C = cond(T,T2).
+flatten_bot(T,T,[]) :-
+  T = var(_,_).
+
+fresh_var(var("fresh",[])).
+
+flatten_conds([],[]).
+flatten_conds([C|Cs],[C2|Cs3]) :-
+  flatten_cond(C,C2,NewCs),
+  append(Cs,NewCs,Cs2),
+  flatten_conds(Cs2,Cs3).
+
+flatten_cond(cond(X,Y),cond(X2,Y2),Cs) :-
+  flatten_top(X,X2,NewCs),
+  flatten_top(Y,Y2,NewCs2),
+  append(NewCs,NewCs2,Cs).
+
+is_basic(fun(_,Ts)) :-
+  is_cons_list(Ts).
+
+is_cons_list([]).
+is_cons_list([T|Ts]) :-
+  is_cons(T),
+  is_cons_list(Ts).
+
 is_cons(var(_,_)).
+is_cons(cons(_,[])).
+is_cons(cons(_,[T|Ts])) :-
+  is_cons_list([T|Ts]).
