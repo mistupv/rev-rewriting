6eed6e21c8a4c18b6565755a9efd4dfa892fb04f
Baha <ukonat@gmail.com>
2016-10-24 21:24:07 +0900

    Updated test cases for unify with outputs

--- unitest.pl
+++ unitest.pl
@@ -1,16 +1,16 @@
-unify([(cons("a",[]),cons("a",[]))],X).
-unify([(cons("a",[]),cons("b",[]))],X).
-unify([(var("x",[]),var("x",[]))],X).
-unify([(var("x",[]),cons("a",[]))],X).
-unify([(var("x",[]),var("y",[]))],X).
-unify([(fun("f",[cons("a",[]),var("x",[])]),fun("f",[cons("a",[]),cons("b",[])]))],X).
-unify([(fun("f",[cons("a",[])]),fun("g",[cons("a",[])]))],X).
-unify([(fun("f",[var("x",[])]),fun("f",[var("y",[])]))],X).
-unify([(fun("f",[var("x",[])]),fun("g",[var("y",[])]))],X).
-unify([(fun("f",[var("x",[])]),fun("f",[var("y",[]),var("z",[])]))],X).
-unify([(fun("f",[fun("g",[var("x",[])])]),fun("f",[var("y",[])]))],X).
-unify([(fun("f",[fun("g",[var("x",[])]),var("x",[])]),fun("f",[var("y",[]),cons("a",[])]))],X).
-unify([(var("x",[]),fun("f",[var("x",[])]))],X).
-unify([(var("x",[]),var("y",[])),(var("y",[]),cons("a",[]))],X).
-unify([(cons("a",[]),var("y",[])),(var("x",[]),var("y",[]))],X).
-unify([(var("x",[]),cons("a",[])),(cons("b",[]),var("x",[]))],X).
+unify([(cons("a",[]),cons("a",[]))],success([])).
+unify([(cons("a",[]),cons("b",[]))],failure).
+unify([(var("x",[]),var("x",[]))],success([])).
+unify([(var("x",[]),cons("a",[]))],success([(var("x",[]),cons("a",[]))])).
+unify([(var("x",[]),var("y",[]))],success([(var("x",[]), var("y",[]))])).
+unify([(fun("f",[cons("a",[]),var("x",[])]),fun("f",[cons("a",[]),cons("b",[])]))],success([(var("x",[]),cons("b",[]))])).
+unify([(fun("f",[cons("a",[])]),fun("g",[cons("a",[])]))],failure).
+unify([(fun("f",[var("x",[])]),fun("f",[var("y",[])]))],success([(var("x",[]),var("y",[]))])).
+unify([(fun("f",[var("x",[])]),fun("g",[var("y",[])]))],failure).
+unify([(fun("f",[var("x",[])]),fun("f",[var("y",[]),var("z",[])]))],failure).
+unify([(fun("f",[fun("g",[var("x",[])])]),fun("f",[var("y",[])]))],success([(var("y",[]),fun("g",[var("x",[])]))])).
+unify([(fun("f",[fun("g",[var("x",[])]),var("x",[])]),fun("f",[var("y",[]),cons("a",[])]))],success([(var("y", []),fun("g",[cons("a",[])])),(var("x",[]),cons("a",[]))])).
+unify([(var("x",[]),fun("f",[var("x",[])]))],success([(var("x",[]),fun("f",[var("x",[])]))])).
+unify([(var("x",[]),var("y",[])),(var("y",[]),cons("a",[]))],success([(var("x",[]),cons("a",[])),(var("y",[]),cons("a",[]))])).
+unify([(cons("a",[]),var("y",[])),(var("x",[]),var("y",[]))],success([ (var("x", []), cons("a", [])), (var("y", []), cons("a", []))])).
+unify([(var("x",[]),cons("a",[])),(cons("b",[]),var("x",[]))],failure).
